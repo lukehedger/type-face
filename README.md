4746bb14c981718e7c75d9da55ec27bf631a84fb
lukehedger <luke.hedger@gmail.com>
2019-08-16 14:11:28 +0100

    Add TypeScript example

--- README.md
+++ README.md
@@ -3,19 +3,22 @@
 Experimenting with type-safety in JavaScript GraphQL applications.
 
 ## Comparison
-| Language | Invalid use of response | Invalid argument key | Invalid argument value | Invalid operation name |
-| :--- | :--- | :--- | :--- | :--- |
-| JavaScript | Runtime | Build | Build | Build |
-| Flow | Build | Runtime | Runtime | Runtime |
-| TypeScript | - | - | - | - |
-| Reason | - | - | - | - |
+
+| Language   | Invalid use of response | Invalid argument key | Invalid argument value | Invalid operation name |
+| :--------- | :---------------------- | :------------------- | :--------------------- | :--------------------- |
+| JavaScript | Runtime                 | Build                | Build                  | Build                  |
+| Flow       | Build                   | Runtime              | Runtime                | Runtime                |
+| TypeScript | Build                   | Runtime              | Runtime                | Runtime                |
+| Reason     | -                       | -                    | -                      | -                      |
 
 ## Build examples
+
 ```sh
 yarn build
 ```
 
 ## Run examples
+
 ```sh
 yarn start
 ```
--- examples/typescript/src/index.ts
+++ examples/typescript/src/index.ts
@@ -1,1 +1,56 @@
-console.log("TypeScript For Life");
+import fetch from "node-fetch";
+
+interface Query {
+  hello: string;
+}
+
+interface QueryHelloArgs {
+  name: string;
+}
+
+interface QueryHelloResponse {
+  data: {
+    hello: string;
+  };
+}
+
+// GraphQL request factory
+async function makeRequest(query: string) {
+  const response = await fetch("http://localhost:4000/", {
+    body: JSON.stringify({ query: query }),
+    headers: { "Content-Type": "application/json" },
+    method: "POST"
+  });
+
+  const json = await response.json();
+
+  if (typeof json.errors !== "undefined") {
+    return json.errors.map((error: any) => console.log(error.message));
+  }
+
+  return json;
+}
+
+// Valid operation
+// => Hello TypeScript!
+makeRequest('{hello(name: "TypeScript")}').then((res: QueryHelloResponse) => {
+  console.log(res.data.hello);
+});
+
+// Invalid use of response
+// => undefined
+makeRequest('{hello(name: "TypeScript")}').then((res: QueryHelloResponse) => {
+  console.log(res.data.something);
+});
+
+// Invalid argument key
+// => Unknown argument "username" on field "hello" of type "Query". Did you mean "name"?
+makeRequest('{hello(username: "TypeScript")}');
+
+// Invalid argument value
+// => Expected type String, found 123.
+makeRequest("{hello(name: 123)}");
+
+// Invalid operation name
+// => Cannot query field "hey" on type "Query".
+makeRequest('{hey(name: "TypeScript")}');
